5da26a6245059393a9a07218f7584fb5d8865368
Andrew2432 <drew.devops@gmail.com>
2020-05-29 15:18:32 +0530

    Create layout

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 require('dotenv').config();
 
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 
 const app = express();
 
+// Template engine
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+// Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
+app.use(expressLayouts);
+
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.send('Hello'));
+app.get('/', (req, res) => res.render('welcome'));
 
 app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
-
-
