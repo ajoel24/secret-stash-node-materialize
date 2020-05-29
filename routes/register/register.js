aecc99bbe65d6b239aa4aa4cf37f5bd3081c8c1c
Andrew2432 <drew.devops@gmail.com>
2020-05-29 19:03:59 +0530

    Add validation

--- routes/register/register.js
+++ routes/register/register.js
@@ -7,7 +7,30 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  console.log(req.body);
+  const { email, password, password2 } = req.body;
+  const errors = [];
+
+  // Validation checks
+  if (!email || !password || !password2) {
+    errors.push({ message: 'Please enter all fields' });
+  }
+
+  if (password !== password2) {
+    errors.push({ message: 'Passwords do not match' });
+  }
+
+  if (password.length < 6) {
+    errors.push({ message: 'Password should be atleast 6 characters' });
+  }
+
+  if (errors.length > 0) {
+    res.render('register', {
+      errors,
+      email,
+      password,
+      password2,
+    });
+  }
 });
 
 module.exports = router;
